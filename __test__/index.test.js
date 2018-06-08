337757fbc11ea55ffce9832c7680c01e0a4bfd15
Aaron Reisman <aaron@lifeiscontent.net>
2018-06-09 01:52:00 +0700

    WIP

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,4 +1,4 @@
-import { renderIntoDocument, wait } from "react-testing-library";
+import { render } from "react-testing-library";
 import React from "react";
 import sinon from "sinon";
 import { Mutate } from "../src/Mutate";
@@ -9,6 +9,14 @@ describe("index", () => {
   let container;
   let xhr;
   let requests = [];
+  const TestImpl = props => (
+    <ReactSvgInjector
+      src="https://rawgit.com/lifeiscontent/react-svg-injector/master/__fixtures__/logo.svg"
+      {...props}
+    >
+      <Mutate selector="g" fill="red" />
+    </ReactSvgInjector>
+  );
 
   beforeEach(() => {
     container = document.body.appendChild(document.createElement("div"));
@@ -34,13 +42,19 @@ describe("index", () => {
 
   // @TODO: until this is released, we can't actually test the API changes: https://github.com/airbnb/enzyme/pull/1513
   describe("integration", () => {
-    it("renders an SVG", async () => {
-      const { container, debug } = renderIntoDocument(
-        <ReactSvgInjector src="../__fixtures__/logo.svg">
-          <Mutate fill="red" selector="g" />
-        </ReactSvgInjector>
+    test("react-testing-library works!", done => {
+      const { container, debug } = render(
+        <TestImpl
+          callback={svg => {
+            console.log(svg);
+            done();
+          }}
+        />
       );
-      expect(container).toMatchSnapshot();
+      expect(container.getElementsByTagName("svg")).toHaveLength(1);
+      expect(container.getElementsByTagName("g").getAttribute("fill")).toBe(
+        "red"
+      );
     });
   });
 });
